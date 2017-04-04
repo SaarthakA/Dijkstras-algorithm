39f7129636ec2fee0610b5fa9e573f7a89387bda
Saarthak <Saarthak>
2017-04-04 10:42:51 -0700

    City Class

---
+++ City.java
@@ -0,0 +1,17 @@
+//city class to hold information
+public class City {
+	public int Number;
+	public String City_Code;
+	public String Full_City_Name;
+	public int Population;
+	public int Elevation;
+	public City(int num, String code, String name, int pop, int elev)
+	{
+		Number = num;
+		City_Code = code;
+		Full_City_Name = name;
+		Population = pop;
+		Elevation = elev;
+	}
+
+}
